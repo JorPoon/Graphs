1f9cc2184ab1032e16a6f80dfc275c27e141e08c
Jor_Poon <jormpoon@gmail.com>
2019-06-19 02:33:29 -0400

    imported deque

---
+++ projects/ancestor/ancestor.py
@@ -0,0 +1,15 @@
+from collections import deque
+'''
+Edge Cases:
+Returns -1 if no parents
+No repetition, only one path to parents
+'''
+
+
+
+def earliest_ancestor(input):
+    #
+    #returns -1 if no parent, output = -1
+    #if earliest ancetor is tied returns smaller num
+    #earliest ancestor as return value
+    pass
